5466b176c6354908846898427e56e1d8faaa5cca
cedric <cedric@a2.b4d8.fr>
2022-12-03 13:13:10 +0100

    solve day 3 and add puzzle trait

--- utils/target/debug/deps/aoc-27fbe898235f91d2.d
+++
@@ -1,5 +0,0 @@
-/home/cedric/Projets/b4D8/aoc/aoc/target/debug/deps/aoc-27fbe898235f91d2.rmeta: src/lib.rs
-
-/home/cedric/Projets/b4D8/aoc/aoc/target/debug/deps/aoc-27fbe898235f91d2.d: src/lib.rs
-
-src/lib.rs:
--- utils/target/debug/deps/aoc-d639ab292e5f84e4.d
+++
@@ -1,5 +0,0 @@
-/home/cedric/Projets/b4D8/aoc/aoc/target/debug/deps/aoc-d639ab292e5f84e4.rmeta: src/lib.rs
-
-/home/cedric/Projets/b4D8/aoc/aoc/target/debug/deps/aoc-d639ab292e5f84e4.d: src/lib.rs
-
-src/lib.rs:
